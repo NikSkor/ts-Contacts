170277321af6fc868404e78ca2e0d5eb61f2b71b
NikSkor <mr-zerx@yandex.ru>
2022-10-10 15:30:17 +0500

    TypeGuards added

--- src/TS-Contacts.ts
+++ src/TS-Contacts.ts
@@ -1,10 +1,12 @@
 interface User {
+  type: 'user';
   name: string;
   age: number;
   group: string;
 };
 
 interface Admin {
+  type: 'admin';
   name: string;
   age: number;
   role: string;
@@ -14,46 +16,65 @@ type Person = User | Admin;
 
 const persons: Person[] = [
   {
+    type: 'admin',
     name: 'Пётр Измайлов',
     age: 29,
     role: 'Администратор',
   },
   {
+    type: 'user',
     name: 'Иван Петров',
     age: 27,
     group: 'SEO-специалист',
   },
   {
+    type: 'user',
     name: 'Марат Aляуддинов',
     age: 20,
     group: 'Музыкант',
   },
   {
+    type: 'user',
     name: 'Илай Венс',
     age: 47,
     group: 'Коллега по работе',
   },
   {
+    type: 'user',
     name: 'Игнат Пришт',
     age: 37,
     group: 'Семья',
   },
   {
+    type: 'user',
     name: 'Ульяна Романова',
     age: 41,
     group: 'Координатор',
   }
 ];
 
+const isAdmin = (person: Person): person is Admin => {
+  return person.type === 'admin';
+}
+
+const isUser = (person: Person): person is User => {
+  return person.type === 'user';
+}
+
 const logPerson = (person: Person): void => {
-  let information: string;
-  if('role' in person) {
+  let information: string = '';
+  if(isAdmin(person)) {
     information = person.role;
-  } else {
+  } 
+  if (isUser(person)) {
     information = person.group;
   }
   console.log(`${person.name}, ${person.age}, ${information}`);
 }
+console.log('Admins:');
+persons.filter(isAdmin).forEach(logPerson);
+
+console.log();
 
 console.log('Users:');
-persons.forEach(logPerson);
+persons.filter(isUser).forEach(logPerson);
